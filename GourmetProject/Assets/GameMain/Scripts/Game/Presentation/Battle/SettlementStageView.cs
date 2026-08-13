cb9501eb6ae65daee82abccac1e0f01c6f6522ce
yijin <359222976@qq.com>
2026-08-13 20:34:02 +0800

    *

--- GourmetProject/Assets/GameMain/Scripts/Game/Presentation/Battle/SettlementStageView.cs
+++ GourmetProject/Assets/GameMain/Scripts/Game/Presentation/Battle/SettlementStageView.cs
@@ -93,7 +93,7 @@ namespace GourmetProject.Game.Presentation.Battle
 
                 await SpawnLabelAsync(
                     anchor,
-                    ReadableName(sourceName, "效果触发"),
+                    ReadableName(sourceName, "技能触发"),
                     effectText,
                     theme,
                     duration,
@@ -177,7 +177,7 @@ namespace GourmetProject.Game.Presentation.Battle
 
             await SpawnLabelAsync(
                 anchor,
-                group.Trace?.Kind == SkillExecutionKind.SweetTransfer ? "执行技能" : "效果触发",
+                "技能触发",
                 group.Trace?.Kind == SkillExecutionKind.SweetTransfer
                     ? ReadableName(group.Trace.SkillName, group.SourceName)
                     : group.SourceName,
