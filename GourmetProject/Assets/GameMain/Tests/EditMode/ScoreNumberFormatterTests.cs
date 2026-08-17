6f5e7e4cabcf6020c6a065ccc5d9c2f91f412a83
yijin <359222976@qq.com>
2026-08-17 19:22:59 +0800

    *

--- GourmetProject/Assets/GameMain/Tests/EditMode/ScoreNumberFormatterTests.cs
+++ GourmetProject/Assets/GameMain/Tests/EditMode/ScoreNumberFormatterTests.cs
@@ -10,33 +10,33 @@ namespace GourmetProject.Tests.EditMode
         [TestCase(0d, "0")]
         [TestCase(9d, "9")]
         [TestCase(1234d, "1234")]
-        [TestCase(999_999_999d, "999999999")]
-        public void Format_NineDigitsOrFewer_KeepsFullInteger(double value, string expected)
+        [TestCase(9_999_999d, "9999999")]
+        public void Format_SevenDigitsOrFewer_KeepsFullInteger(double value, string expected)
         {
             Assert.That(ScoreNumberFormatter.Format(value), Is.EqualTo(expected));
         }
 
         [Test]
-        public void Format_TenDigits_UsesScientificNotation()
+        public void Format_EightDigits_UsesScientificNotation()
         {
-            Assert.That(ScoreNumberFormatter.Format(1_000_000_000d), Is.EqualTo("1e9"));
-            Assert.That(ScoreNumberFormatter.Format(1_230_000_000d), Is.EqualTo("1.23e9"));
+            Assert.That(ScoreNumberFormatter.Format(10_000_000d), Is.EqualTo("1e7"));
+            Assert.That(ScoreNumberFormatter.Format(12_300_000d), Is.EqualTo("1.23e7"));
         }
 
         [Test]
-        public void FormatNumber_NineDigitsOrFewer_DoesNotUseGeneralScientific()
+        public void FormatNumber_SevenDigitsOrFewer_DoesNotUseGeneralScientific()
         {
             Assert.That(FoodTipUiUtility.FormatNumber(new BigDouble(1234d)), Is.EqualTo("1234"));
-            Assert.That(FoodTipUiUtility.FormatNumber(new BigDouble(999_999_999d)), Is.EqualTo("999999999"));
+            Assert.That(FoodTipUiUtility.FormatNumber(new BigDouble(9_999_999d)), Is.EqualTo("9999999"));
             Assert.That(FoodTipUiUtility.FormatNumber(new BigDouble(1.5d)), Is.EqualTo("1.5"));
         }
 
         [Test]
-        public void FormatNumber_TenDigits_UsesScientificNotation()
+        public void FormatNumber_EightDigits_UsesScientificNotation()
         {
             Assert.That(
-                FoodTipUiUtility.FormatNumber(new BigDouble(1_000_000_000d)),
-                Is.EqualTo("1e9"));
+                FoodTipUiUtility.FormatNumber(new BigDouble(10_000_000d)),
+                Is.EqualTo("1e7"));
         }
     }
 }
